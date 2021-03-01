08820019a99c13f053eeee4400aec2781af1e04e
JulianIlla <jilla23211@columbushs.com>
2021-03-01 09:19:14 -0500

    5th video done

--- weather app/scripts/forecast.js
+++ weather app/scripts/forecast.js
@@ -1,5 +1,16 @@
 const key = '	xvpOBAAypFh84YftzPvUCh8ZM80gbYIG';
 
+const getWeather = async (id) => {
+  
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
 const getCity = async (city) => {
 
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
@@ -11,6 +22,8 @@ const getCity = async (city) => {
   return data[0];
 };
 
-getCity('manchester')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+getCity('manchester').then(data => {
+  return getWeather(data.Key);
+}).then(data => {
+  console.log(data);
+}).catch(err => console.log(err));
